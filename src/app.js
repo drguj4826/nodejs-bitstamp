426d76ec9cd54326ceb9747412751cf7de781a04
L <drguj4826@gmail.com>
2023-06-10 23:35:20 +0800

    calculate average

--- src/app.js
+++ src/app.js
@@ -10,9 +10,21 @@ https.get(url, (res) => {
   });
 
   res.on('end', () => {
-    console.log(data);
+    const jsonData = JSON.parse(data);
+    const prices = Object.values(jsonData).map(item => parseFloat(item.last));
+    const average = calculateAverage(prices);
+    console.log('Average last price:', average);
   });
 
 }).on("error", (err) => {
   console.log("Error: " + err.message);
 });
+
+function calculateAverage(arr) {
+  if (arr.length === 0) {
+    return 0;
+  }
+
+  const sum = arr.reduce((total, value) => total + value, 0);
+  return sum / arr.length;
+}
